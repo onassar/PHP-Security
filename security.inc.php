884e74474bda7b49e56aec0b30b1242a72d4f744
Oliver Nassar <onassar@gmail.com>
2012-11-19 17:44:20 -0500

    Adding slugging function, which is often used to secure requests and resources

--- security.inc.php
+++ security.inc.php
@@ -35,3 +35,33 @@
         }
         return htmlentities($mixed, ENT_QUOTES, 'UTF-8', false);
     }
+
+    /**
+     * slugify
+     * 
+     * Turns a string into a url-friendly string (alphabumeric).
+     * 
+     * @access public
+     * @param  String $str
+     * @param  Boolean $limit. (default: 100)
+     * @param  Boolean $lowercase. (default: true)
+     * @return String
+     */
+    function slugify($str, $limit = 100, $lowercase = true)
+    {
+        $str = decode($str);
+        $str = trim($str);
+        if ($lowercase === true) {
+            $str = strtolower($str);
+        }
+        $str = str_replace('\'', '', $str);
+        $str = str_replace('&amp;', '&', $str);
+        $str = str_replace('&', '-and-', $str);
+        $str = preg_replace('/[^a-zA-Z0-9-]/', ' ', $str);
+        $str = str_replace('-', ' ', $str);
+        $str = preg_replace('/[\s]{2,}/', ' ', $str);
+        $str = trim($str);
+        $str = str_replace(' ', '-', $str);
+        $str = substr($str, 0, $limit);
+        return $str;
+    }
